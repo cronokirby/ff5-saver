c9080a20676172cc1b1d8ade22319e1a477b137c
cronokirby <cronokirby@gmail.com>
2017-08-20 16:25:49 +0200

    Adds a helper method for byte slicing

---
+++ FF5Saver/Data/ByteHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FF5Saver.Data
+{
+    public class ByteHelper
+    {
+        public static byte[] SliceBytes(byte[] array, int start = 0, int stop = 0, bool littleEndian = false)
+        {
+            if (stop <= 0)
+            {
+                stop = array.Length - stop;
+            }
+            byte[] buffer = new byte[stop - start];
+            Buffer.BlockCopy(array, start, buffer, 0, stop - start);
+            if (littleEndian)
+            {
+                Array.Reverse(buffer);
+            }
+            return buffer;
+        }
+    }
+}
+
